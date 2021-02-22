f3d113bcad567d8147642461174fe9a1dfd1f0f9
Rutuja9696 <rutuja.patil@contentstact.com>
2021-02-22 12:58:56 +0530

    added thunk middleware

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 // import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducer/root.reducer";
-const store = createStore(rootReducer);
+import fetchBlogs from "../middleware/fetchBlog";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(fetchBlogs, thunk));
 export default store;
