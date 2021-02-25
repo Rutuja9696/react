159ba10350571515d9766061861693f4de0b2a29
Rutuja9696 <rutuja.patil@contentstact.com>
2021-02-25 17:13:17 +0530

    removed comments

--- src/redux/saga/fetchBlogs.saga.js
+++ src/redux/saga/fetchBlogs.saga.js
@@ -1,34 +1,19 @@
 import axios from "axios";
 import { call, put, takeEvery } from "redux-saga/effects";
 import { blogActionTypes } from "../constants/blogActionTypes";
-// import { takeLatest, put } from "redux-saga/effects";
-// import { blogActionTypes } from "../constants/blogActionTypes";
 const url = "https://blog-rendering1.herokuapp.com/blogs";
-// function* fetchBlogsSaga() {
-//   //   console.log("Example saga reached");
-//   const blogResponse = yield fetch(url);
-//   const blogs = yield blogResponse.json();
-// }
-// export default function* watchFetchBlogsSaga() {
-//   yield takeLatest(blogActionTypes.GET, fetchBlogsSaga);
-// }
+
 const fetchBlogs = async () => {
   const response = await axios.get(url);
   return response.data.data;
 };
 export function* fetchBlogsSaga(payload) {
   try {
-    // yield call(startFetchFun());
-
     const response = yield call(fetchBlogs);
     if (response) {
       yield put({ type: blogActionTypes.GET, payload: { blogs: response } });
     }
-    // yield call(processFetchFun(response.data));
-    // .then((response) => {
-    // })
   } catch (err) {
-    // yield call(endFetchFun(error.message));
     console.log(err.message);
   }
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,19 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducer/root.reducer";
-// import fetchBlogs from "../middleware/fetchBlog";
 import thunk from "redux-thunk";
 import { fetchBlogsSaga } from "../saga/fetchBlogs.saga";
 import createSagaMiddleware from "redux-saga";
-// import watchFetchBlogsSaga from "../saga/fetchBlogs.saga";
 const url = "https://blog-rendering1.herokuapp.com/blogs";
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  rootReducer,
-  // applyMiddleware(fetchBlogs, thunk)
-  applyMiddleware(sagaMiddleware)
-);
-// sagaMiddleware.run(watchFetchBlogsSaga);
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(fetchBlogsSaga);
 export default store;
