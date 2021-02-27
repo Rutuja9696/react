4ce22027910d9e4ea40e7e0b1e90308b99ac6af7
Rutuja9696 <rutuja.patil@contentstact.com>
2021-02-27 15:58:45 +0530

    added function to toggle theme,state to store theme,theme context provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import "./App.css";
+// import "./App.css";
 import Blogs from "./components/blogs";
 import store from "./redux/store/store";
 import { Provider } from "react-redux";
+import { useState } from "react";
+import ThemeContext, { themes } from "./theme-context";
 
 function App() {
+  const [theme, setTheme] = useState(themes.dark);
+  const toggleTheme = () =>
+    theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
   return (
     <Provider store={store}>
-      <div className="App">
-        <Blogs />
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <div className="App">
+          <Blogs toggleTheme={toggleTheme} />
+        </div>
+      </ThemeContext.Provider>
     </Provider>
   );
 }
