4a5d3b7511af5109cb1a2ff1091962b4149a28c6
Rutuja9696 <rutuja.patil@contentstact.com>
2021-03-02 20:28:26 +0530

    added loader

--- src/components/blogs.jsx
+++ src/components/blogs.jsx
@@ -5,6 +5,7 @@ import { blogActionTypes } from "../redux/constants/blogActionTypes";
 import blogActionObjectGenerator from "../redux/action/blogActionGenerator";
 import Styles from "../styles/blog.module.css";
 import ThemeContext from "../theme-context";
+import Loader from "react-loader-spinner";
 
 function Blogs(props) {
   //using of hook
@@ -18,19 +19,23 @@ function Blogs(props) {
     <div style={themes}>
       <button onClick={props.toggleTheme}>Change theme</button>
       <h1>BLOGS</h1>
-      <div className={Styles.container}>
-        {props.blogProps.map((blog, i) => {
-          return (
-            <div key={i} className={Styles.card}>
-              <img src={blog.imageUrl} className={Styles.image} />
-              <p key={blog.id} className={Styles.title}>
-                {blog.title}
-              </p>
-              <h6 className={Styles.author}>- {blog.author}</h6>
-            </div>
-          );
-        })}
-      </div>
+      {props.blogProps.length === 0 ? (
+        <Loader type="Puff" color="#00BFFF" height={300} width={80} />
+      ) : (
+        <div className={Styles.container}>
+          {props.blogProps.map((blog, i) => {
+            return (
+              <div key={i} className={Styles.card}>
+                <img src={blog.imageUrl} className={Styles.image} />
+                <p key={blog.id} className={Styles.title}>
+                  {blog.title}
+                </p>
+                <h6 className={Styles.author}>- {blog.author}</h6>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
